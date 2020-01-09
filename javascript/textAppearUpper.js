3feb51c258153038a2174bb972ab3215ab4a0ab8
MeesMD <meesmilan@hotmail.com>
2020-01-09 13:40:32 +0100

    Changed main page background and some other fixes

--- javascript/textAppearUpper.js
+++ javascript/textAppearUpper.js
@@ -1,29 +1,29 @@
-$(window).bind("load", function() {
+$(window).bind("load", function () {
 
-$('.ml11 .letters').each(function(){
-  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-});
+  $('.ml11 .letters').each(function () {
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
 
-anime.timeline({loop: false})
+  anime.timeline({ loop: false })
 
-  .add({
-    translateX: [0,$(".ml11 .letters").width()],
-    easing: "easeOutExpo",
-    duration: 1000,
-    delay: 100
-  }).add({
-    targets: '.ml11 .letter',
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=775',
-    delay: function(el, i) {
-      return 34 * (i+1)
-    }
-  }).add({
-    targets: '.ml11 .line',
-    opacity: 0,
-    duration: 200,
-    easing: "easeOutExpo",
-  })
+    .add({
+      translateX: [0, $(".ml11 .letters").width()],
+      easing: "easeOutExpo",
+      duration: 1000,
+      delay: 100
+    }).add({
+      targets: '.ml11 .letter',
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=775',
+      delay: function (el, i) {
+        return 34 * (i + 1)
+      }
+    }).add({
+      targets: '.ml11 .line',
+      opacity: 0,
+      duration: 200,
+      easing: "easeOutExpo",
+    })
 });
